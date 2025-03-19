0bcb59282fd0b6c699916b5a0e6b3c2870049624
dasava11 <dasava11@outlook.com>
2025-03-19 09:57:17 -0500

    limpieza de espacios y codigo sin usar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import {RouterModule, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { TypeUserService } from './services/typeUser/type-user.service';
-import { PurchasesService } from './services/purchases/purchases.service';
-import { ShoppingService } from './services/shopping/shopping.service';
-import { CustomerService } from './services/customer/customer.service';
-import { ProductService } from './services/product/product.service';
-import { SupplierService } from './services/supplier/supplier.service';
-import { UsersService } from './services/users/users.service';
-
-
-
-
-
 @Component({
     selector: 'app-root',
     standalone: true,
@@ -24,15 +12,6 @@ import { UsersService } from './services/users/users.service';
 
 })
 export class AppComponent {
-  constructor(
-    private shoppingService: ShoppingService, 
-    private customerService: CustomerService, 
-    private purchasesService: PurchasesService,
-    private productService: ProductService, 
-    private supplierService: SupplierService, 
-    private typeUserService: TypeUserService, 
-    private userService: UsersService
-  ) {}
 }
 
 
