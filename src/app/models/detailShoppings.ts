b41f515a9ef1a41beb58501ffad8697098c98f7c
David Sánchez <115512145+dasava11@users.noreply.github.com>
2025-03-19 20:00:13 -0500

    David cash register (#17)
    * Ajuste marco en caja registradora
    
    * HAsta acá por hoy
    
    ---------
    
    Signed-off-by: David Sánchez <115512145+dasava11@users.noreply.github.com>

--- src/app/models/detailShoppings.ts
+++ src/app/models/detailShoppings.ts
@@ -2,6 +2,7 @@ export interface DetailShoppings {
  /*  id_detail_shopping: string;
   id_shopping: number; */
   id_products: number
+  code?: string;  
   count: number;
   unit_price: number;
   value_taxes: number;
